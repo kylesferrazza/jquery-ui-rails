76f82f243be40e0f99b2f2c509f73cf67cdc92d7
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2020-01-06 15:03:35 -0500

    update versions

--- lib/jquery/ui/rails/version.rb
+++ lib/jquery/ui/rails/version.rb
@@ -1,8 +1,8 @@
 module Jquery
   module Ui
     module Rails
-      VERSION = "6.0.1"
-      JQUERY_UI_VERSION = "1.12.1"
+      VERSION = "6.1.0"
+      JQUERY_UI_VERSION = "1.12.2-pre"
     end
   end
 end
